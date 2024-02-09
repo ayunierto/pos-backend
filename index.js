ec753b0ffedea668c962e51e369f721a736982bc
Alcides Yunier Turruellas Osorio <ayunierto@gmail.com>
2024-02-09 04:59:31 +0100

    config environment vars

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const express = require('express');
-
+require('dotenv').config();
 
 // Crear servidor de express
 const app = express();
 
+app.use( express.static('public'));
 
-app.get('/', ( req, res ) => {
+// app.get('/', ( req, res ) => {
 
-    res.json({
-        ok: true
-    });
-});
+//     res.json({
+//         ok: true
+//     });
+// });
 
 
 // Escuchar peticiones
-app.listen( 4000, () => {
-    console.log(`Server on port ${ 4000 }`);
+app.listen( process.env.PORT, () => {
+    console.log(`Server on port ${ process.env.PORT }`);
 });
