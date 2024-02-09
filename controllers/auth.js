c7a767fcca2fe730f0b32d166ab34ae3bad71023
Alcides Yunier Turruellas Osorio <ayunierto@gmail.com>
2024-02-09 05:48:18 +0100

    recover information

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,30 @@
+const express = require('express');
+
 const createUser = ( req, res ) => {
-    res.json({
+    const { name, email, password } = req.body;
+
+    if (name.length < 5 ) {
+        return res.status(400).json({
+            ok: false,
+            msg: 'The name must be 5 letters'
+        });
+    }
+    res.status(201).json({
         ok: true,
-        msg: 'register'
+        msg: 'register',
+        name,
+        email, 
+        password
     });
 };
 
 const loginUser = ( req, res ) => {
+    const { email, password } = req.body;
     res.json({
         ok: true,
-        msg: 'login'
+        msg: 'login',
+        email, 
+        password
     });
 };
 
--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 
 app.use( express.static('public'));
 
+app.use( express.json() );
+
 // Routes
 app.use('/api/auth', require('./routes/auth'));
 
