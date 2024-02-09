3a05ab16db0b35e8c3150db97e4ddbf7a20a9969
Alcides Yunier Turruellas Osorio <ayunierto@gmail.com>
2024-02-09 04:21:42 +0100

    express config

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Hola Mundo!!');
+const express = require('express');
+
+
+// Crear servidor de express
+const app = express();
+
+
+app.get('/', ( req, res ) => {
+
+    res.json({
+        ok: true
+    });
+});
+
+
+// Escuchar peticiones
+app.listen( 4000, () => {
+    console.log(`Server on port ${ 4000 }`);
+});
